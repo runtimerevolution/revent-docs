9edc31d46a3a993bb8dc5fa4ce46cb16e387f65e
João Veríssimo <57098650+JoaoCVerissimo@users.noreply.github.com>
2022-04-13 11:43:09 +0100

    Jv/task/add api creation (#1)

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # revent-docs
+
 Docs to document the creation of the app and api for the Runtime Revolution revent.
+
+Here you can find an api folder with documentation to *How we created the app*.
+
+How did i create this docs page?
+
+  1. Clone repository
+  
+  2. yarn add next nextra react react-dom nextra-theme-docs
+  
+  3. Start creating your next.config.js file and [configure it](https://nextra.vercel.app/get-started)
+
+  4. Create your theme.config.js file and [configure it](https://nextra.vercel.app/themes/docs/configuration)
+
+  5. Create your pages folder with _app.js [configured file](https://nextra.vercel.app/get-started) and an index.mdx
+
+  6. Run yarn install and yarn next
---
+++ theme.config.js
@@ -0,0 +1,24 @@
+export default {
+  projectLink: 'https://github.com/runtimerevolution/revent-docs/',
+  docsRepositoryBase: 'https://github.com/runtimerevolution/revent-docs',
+  titleSuffix: " - Revent Docs",
+  nextLinks: true,
+  prevLinks: true,
+  darkMode: true,
+  search: true,
+  footer: true,
+  footerText: `Runtime Revolution Photo Contest ${new Date().getFullYear()}`,
+  logo: (
+    <>
+      <svg>...</svg>
+      <span>Revent Docs</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Revent: How did we build this project" />
+      <meta name="og:title" content="Revent: your best Runtime Event" />
+    </>
+  ),
+}
